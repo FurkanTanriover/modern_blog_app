9dca2d2d1e475f8010de2b681454970ffc79fb5e
Furkan Tanrıöver <furkan.tanrioveer@gmail.com>
2022-10-09 14:20:32 +0300

    author part added

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, {useState,useEffect,useRef} from 'react'
 
-const CommentsForm = () => {
+const CommentsForm = ({slug}) => {
+  const [error, setError] = useState(false)
+  const [localStorage, setLocalStorage] = useState(null)
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false)
+  const commentEl = useRef();
+  const nameEl = useRef();
+  const emailEl = useRef();
+  const storeDataEl = useRef();
+
+
   return (
-    <div>
+    <div className=' bg-white p-8 rounded-lg pb-12 mb-8'>
     <h1>CommentsForm</h1>
     </div>
   )
